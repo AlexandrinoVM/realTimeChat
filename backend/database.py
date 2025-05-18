251bce4360010ef2ac5799df96689b961b6ea953
AlexandrinoVM <vinicius.moraes2@outlook.com>
2025-05-18 19:21:18 -0300

    adding os dotenv on db path

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session,sessionmaker,declarative_base
+import os
+from dotenv import load_dotenv
 
-db = create_engine("sqlite:///backend/database.db")
+load_dotenv()
+db = create_engine(os.getenv('DATABASEPATH'))
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=db)
 #Session = sessionmaker(bind=db)
 #session = Session()
